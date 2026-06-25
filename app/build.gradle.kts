7dbc591548e50d8b3603ed45f9dcc54e279a140d
georstef <georstef@gmail.com>
2026-06-25 23:34:24 +0300

    fix: strengthen prompt to prevent duplicate options in multiple choice questions & new build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.galleriaida"
         minSdk = 26
         targetSdk = 35
-        versionCode = 10000
-        versionName = "1.0.0"
+        versionCode = 10001
+        versionName = "1.0.1"
     }
 
     buildTypes {
