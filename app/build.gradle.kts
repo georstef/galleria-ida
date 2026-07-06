bf0b709a05619492cc9ae59042ee76adca233244
George S. <georstef@gmail.com>
2026-07-06 21:14:48 +0300

    New version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.galleriaida"
         minSdk = 26
         targetSdk = 35
-        versionCode = 10002
-        versionName = "1.0.2"
+        versionCode = 10003
+        versionName = "1.0.3"
     }
 
     buildTypes {
